154cd48d7790738d0e60d137569ac66d07cc4f01
tosone <i@tosiney.com>
2016-03-26 16:25:55 +0800

    Signed-off-by: tosone <i@tosiney.com>

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-﻿/* global __dirname */
 'use strict';
-var express = require('express');
-var ejs = require('ejs');
-var path = require('path');
-var app = express();
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
-var morgan = require('morgan');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
-var config = require('webconfig.js');
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const config = require('webconfig.js');
 app.use(session({
     store: new MongoStore({
         url: config.mongoURL,
@@ -34,7 +33,9 @@ app.use(session({
 app.use(morgan('dev'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', ejs.__express);
@@ -48,12 +49,12 @@ app.use('/user', require('./routes/user/user'));
 app.use('/code', require('./routes/code'));
 app.use('/test', require('./routes/test'));
 // app.use('/list/code', require('./routes/list/code'));
-http.createServer(app).listen(80, function () {
+http.createServer(app).listen(80, function() {
     console.log('Server runing at http://127.0.0.1:80.');
 });
 https.createServer({
     key: fs.readFileSync('./privatekey.pem'),
     cert: fs.readFileSync('./certificate.pem')
-}, app).listen(443, function () {
+}, app).listen(443, function() {
     console.log('Https server listening at https://127.0.0.1:443.');
 });
