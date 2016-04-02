0b2eb711928f91dec9662f82ed177de52645447c
tosone <i@tosiney.com>
2016-04-02 17:27:31 +0800

    Signed-off-by: tosone <i@tosiney.com>

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-pm2 stop CodeShare
 git reset --hard origin/master  
 git clean -f  
 git pull  
 git checkout master
 cnpm install
-pm2 start app.js -i 0 --name CodeShare
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 "use strict";
-const mongo_express = require('mongo-express/lib/middleware');
-const mongo_express_config = require('../mongo_express_config');
+// const mongo_express = require('mongo-express/lib/middleware');
+// const mongo_express_config = require('../mongo_express_config');
 module.exports = (app) => {
     app.use('/', require('./index'));
     app.use('/login', require('./sign/login'));
@@ -10,5 +10,5 @@ module.exports = (app) => {
     app.use('/code', require('./code'));
     app.use('/test', require('./test'));
     app.use('/comment', require('./comment'));
-    app.use('/mongo', mongo_express(mongo_express_config));
+    // app.use('/mongo', mongo_express(mongo_express_config));
 }
