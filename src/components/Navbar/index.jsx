8c9011a13689b216aafe361c353502e27d91a0e7
Tosone <i@tosiney.com>
2018-08-19 19:11:43 +0800

    :construction: Add user info component.
    Signed-off-by: Tosone <i@tosiney.com>

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Layout, Menu, Icon } from 'antd';
 
 class Navbar extends React.Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  };
+
   render() {
-    return (<div>navbar</div>);
+    return (
+      <Layout.Header>
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+          theme="dark"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}>
+          <Menu.Item key="home">
+            <Icon type="home" />首页
+          </Menu.Item>
+          <Menu.Item key="explor">
+            <Icon type="star" />探索
+          </Menu.Item>
+          <Menu.Item key="ranking">
+            <Icon type="line-chart" />排行榜
+          </Menu.Item>
+        </Menu>
+      </Layout.Header>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './pages/Home';
+
+import 'antd/dist/antd.css';
+import './index.css';
+
+ReactDOM.render(
+  <BrowserRouter basename="/CodeShare">
+    <Switch>
+      <Route path="/" exact component={Home} />
+      {/* <Redirect from="/old-match" to="/will-match" />
+      <Route path="/will-match" component={WillMatch} />
+      <Route component={NoMatch} /> */}
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
